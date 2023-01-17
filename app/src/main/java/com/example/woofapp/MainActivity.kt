a40a13b52cf6e928d602f6b44462689c46f48ac2
Sleyther <sleyther.calsin@cloudcomputing.com.pe>
2023-01-17 16:10:51 -0500

    Add Adnimations and Manage State

--- app/src/main/java/com/example/woofapp/MainActivity.kt
+++ app/src/main/java/com/example/woofapp/MainActivity.kt
@@ -3,6 +3,11 @@ package com.example.woofapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.StringRes
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
@@ -10,7 +15,11 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
-import androidx.compose.runtime.Composable
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
+import androidx.compose.material.icons.filled.ExpandLess
+import androidx.compose.material.icons.filled.ExpandMore
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -21,7 +30,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.woofapp.data.Dog
 import com.example.woofapp.data.dogs
-import com.example.woofapp.ui.theme.WoofAppTheme
+import com.example.woofapp.ui.theme.*
 
 class MainActivity : ComponentActivity() {
  override fun onCreate(savedInstanceState: Bundle?) {
@@ -60,26 +69,60 @@ fun DogItems() {
 }
 
 @Composable
-fun DogItem(dog: Dog) {
+fun DogItem(dog: Dog, modifier: Modifier = Modifier) {
+ var expanded by remember { mutableStateOf(false) };
+
+ val color by animateColorAsState(
+  targetValue = if (expanded) Cyan700 else MaterialTheme.colors.surface
+ )
+
  Card(
   modifier = Modifier.padding(8.dp),
   elevation = 4.dp
  ) {
-  Row(
+  Column(
    modifier = Modifier
-    .fillMaxWidth()
-    .padding(8.dp)
+    .animateContentSize(
+     animationSpec = spring(
+      dampingRatio = Spring.DampingRatioMediumBouncy,
+      stiffness = Spring.StiffnessLow
+     )
+    )
+    .background(color = color)
   ) {
-   Column {
+   Row(
+    modifier = Modifier
+     .fillMaxWidth()
+     .padding(8.dp)
+   ) {
     DogIcon(dog);
+    DogInformation(dog);
+    Spacer(modifier = Modifier.weight(1f))
+    DogItemButton(
+     expanded = expanded,
+     onClick = { expanded = !expanded }
+    );
    }
-   DogInformation(dog);
+   if (expanded) {
+    DogHobby(dogHobby = dog.hobbies)
+   }
   }
  }
 }
 
 @Composable
-fun DogInformation(dog: Dog) {
+fun DogItemButton(expanded: Boolean, onClick: () -> Unit, modifier: Modifier = Modifier) {
+ IconButton(onClick = onClick) {
+  Icon(
+   imageVector = if (expanded) Icons.Filled.ExpandLess else Icons.Filled.ExpandMore,
+   tint = MaterialTheme.colors.secondary,
+   contentDescription = stringResource(id = R.string.expanded_button_content_description)
+  )
+ }
+}
+
+@Composable
+fun DogInformation(dog: Dog, modifier: Modifier = Modifier) {
  Column(
   modifier = Modifier.padding(start = 5.dp)
  ) {
@@ -96,7 +139,28 @@ fun DogInformation(dog: Dog) {
 }
 
 @Composable
-fun DogIcon(dog: Dog) {
+fun DogHobby(@StringRes dogHobby: Int, modifier: Modifier = Modifier) {
+ Column(
+  modifier = modifier.padding(
+   start = 16.dp,
+   top = 8.dp,
+   bottom = 16.dp,
+   end = 16.dp
+  )
+ ) {
+  Text(
+   text = stringResource(id = R.string.about),
+   style = MaterialTheme.typography.h3
+  )
+  Text(
+   text = stringResource(id = dogHobby),
+   style = MaterialTheme.typography.body1
+  )
+ }
+}
+
+@Composable
+fun DogIcon(dog: Dog, modifier: Modifier = Modifier) {
  Image(
   painter = painterResource(id = dog.imageResourceId),
   contentDescription = stringResource(id = dog.name),
@@ -138,7 +202,7 @@ fun DefaultPreview() {
  }
 }
 
-@Preview(showSystemUi = true)
+//@Preview(showSystemUi = true)
 @Composable
 fun DarkThemePreview() {
  WoofAppTheme(darkTheme = true) {
